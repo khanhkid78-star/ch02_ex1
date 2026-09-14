b6d2df5203d20190289ac35c3215b5a2a76fcfae
Van Khanh <khanhkid78@gmail.com>
2026-09-14 15:40:03 +0700

    Disable discount calculation and output formatting
    Commented out discount calculation and formatting output.

--- display_discount.php
+++ display_discount.php
@@ -3,13 +3,14 @@ $product_description = filter_input(INPUT_POST, 'product_description');
 $list_price = filter_input(INPUT_POST, 'list_price');
 $discount_percent = filter_input(INPUT_POST, 'discount_percent');
 
-        $discount = $list_price * $discount_percent * .01;
+        /*$discount = $list_price * $discount_percent * .01;
         $discount_price = $list_price - $discount;
         
         $list_price_format = "$".number_format($list_price, 2);
         $discount_percent_format = $discount_percent."%";
         $discount_format = "$".number_format($discount, 2);
         $discount_price_format = "$".number_format($discount_price, 2);
+        */
 
 ?>
 
@@ -28,16 +29,16 @@ $discount_percent = filter_input(INPUT_POST, 'discount_percent');
         <span><?php echo $product_description; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo $list_price_format; ?></span><br>
+        <span><?php echo $list_price; ?></span><br>
 
         <label>Standard Discount:</label>
         <span><?php echo $discount_percent; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo htmlspecialchars($discount_format); ?></span><br>
+        <span></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo htmlspecialchars($discount_price_format); ?></span><br>
+        <span></span><br>
     </main>
 </body>
 </html>
