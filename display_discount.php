1be6a57f8a70d622a8b62b75f5a9b3d577cff2b1
V Khanh (Work) <khanhkid78@gmail.com>
2026-09-14 16:33:07 +0700

    q11 header

--- display_discount.php
+++ display_discount.php
@@ -24,7 +24,7 @@ $discount_percent = filter_input(INPUT_POST, 'discount_percent');
 </head>
 <body>
     <main>
-        <h1>This page is under construction</h1>
+        <h1>Product Discount Calculator</h1>
 
         <label>Product Description:</label>
         <span><?php echo htmlspecialchars($product_description); ?></span><br>
